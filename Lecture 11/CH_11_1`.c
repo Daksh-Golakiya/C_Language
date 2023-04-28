a1ebac9ea1bdebe15866574c1e7245c942d5b904
Daksh-Golakiya <128048616+Daksh-Golakiya@users.noreply.github.com>
2023-04-28 19:49:59 +0530

    Add files via upload

---
+++ Lecture 11/CH_11_1`.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+cube();
+main()
+{
+	cube();
+}
+cube()
+{
+	int a,b=0;
+	printf("enter the value of cubs : ");
+	scanf("%d",&a);
+	b=a*a*a;
+	printf("the cube of number is : %d",b);
+}
---
+++ Lecture 11/CH_11_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+div();
+main()
+{
+	div();
+}
+div()
+{
+	int a;
+	   printf("enter any number : ");
+	   scanf("%d",&a);
+	   if((a%5)==0 && (a%3))
+	   {
+	   	  printf("enter number is divisible 5 and 3 : ");
+	   }
+	   else
+	   {
+	   	  printf("enter number is divisible 5 and 3 : ");
+	   }
+}
